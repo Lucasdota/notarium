d1a36e71211a5af0351dd84456ff9024ff11b542
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2024-02-12 15:23:22 -0300

    feat/add-whole-crud

--- server/todos.routes.js
+++ server/todos.routes.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const todosRoutes = express.Router();
-
+const {PrismaClient} = require("@prisma/client");
 const allTodos = [{nome: "aaaa", status: false}];
 todosRoutes.post();
 
+const prisma = new PrismaClient();
 
 // C
-todosRoutes.post("/todos", (request, response) => {
-	const { name } = request.body
+todosRoutes.post("/todos", async (request, response) => {
+	const { name } = request.body;
+	const todo = await prisma.todo.create({
+		data: {
+			name,
+		}
+	}) 
 	allTodos.push({name, status: false})
 	return response.status(201).json(allTodos)
 })
 // R
-todosRoutes.get("/todos", (request, response) => {
-	return response.status(200).json(allTodos);
+todosRoutes.get("/todos", async (request, response) => {
+	const todos = await prisma.todo.findMany();
+	return response.status(200).json(todo);
 })
 // U
+todosRoutes.put("/todos", async (request, response) => {
+	const {name, id, status} = request.body;
+	const todo = await prisma.todo.update({
+		where: {
+			id,
+		},
+		data: {
+			name,
+			status
+		}
+	})
+})
 // D
 
 module.exports = todosRoutes;
